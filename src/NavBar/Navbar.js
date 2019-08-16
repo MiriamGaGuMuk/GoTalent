31871c149ae80a86f3c9dd572a03a1ad8a173490
MiriamGaGu <mgg_pjl@yahoo.com>
2019-08-16 12:02:39 -0500

    Working in responsive

--- src/NavBar/Navbar.js
+++ src/NavBar/Navbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Layout,Header,Navigation,Drawer,Textfield} from 'react-mdl';
+import {Link} from 'react-router-dom';
 import './navbar.css';
 import Table from '../Table/Table';
 
@@ -10,10 +11,10 @@ class NavBar extends Component {
                 <Layout fixedHeader>
                     <Header id="nav__bar" title={<span><span style={{ color: '#ddd' }}></span><strong>Go Talent</strong></span>}>
                         <Navigation>
-                            <a href="#"><i class="fas fa-cog"></i></a>
-                            <a href="#"><i class="fas fa-chevron-down"></i></a>
+                            <Link to="/"><i class="fas fa-cog"></i></Link>
+                            <Link to="/"><i class="fas fa-chevron-down"></i></Link>
                             <span id="line__nav">|</span>
-                            <a href="#"><i class="far fa-bell"></i></a>
+                            <Link to="/"><i class="far fa-bell"></i></Link>
                         </Navigation>
                     </Header>
                     <Drawer id="side__bar" style={{height: '100%'}} title="Go Talent">
@@ -29,16 +30,13 @@ class NavBar extends Component {
                         </Navigation>
                     </Drawer>
                     <div className="companies">
-                    <h6 className="add__companies">Directorio de Empresas</h6>
-                    <Textfield className="text__field"
-                        onChange={() => {}}
-                        label="Agregar empresa"
-                        style={{width: '200px'}}
-                    />
+                    <span className="add__companies">Directorio de Empresas</span>
+                    <input placeholder="Agregar empresa"/>
                     </div>
-                    <div>
-                        <p className="companies__status">Empresas</p>
-                        <div className="companies__status"><span>500</span></div>
+                    <div className="companies__numbers">
+                        <span className="companies__status">Empresas</span>
+                        <span className="companies__status">500</span>
+                        <span>Ordenr por: <a href="#">Empresa</a> | <a href="#">Administrador</a></span>
                     </div>
                     <Table/>
                 </Layout>
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -11,29 +11,56 @@ class Table extends Component{
                     <span><i class="fas fa-arrow-circle-left"></i></span>
                     <span><i class="fas fa-arrow-circle-right"></i></span>
                 </div>
-        <DataTable id="data__table"
+                <div className="navigation__companies">
+                    <input style={{textAlign: "left"}} placeholder="Search"/>
+                    <span><i class="far fa-star"></i></span>
+                    <span>A B <strong>C</strong> D E F G H I J K L M N O P Q R S T U V W X Y Z</span>          
+                </div>
+        <div id="data__table__container" >
+        <DataTable col={8} tablet={6} id="data__table"
             selectable
             shadow={0}
             rowKeyColumn="id"
             rows={[
-                {id: 1001, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '24 Ene', Usuarios: 290, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1002, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '5 Abr', Usuarios: 125, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
-                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '10 Feb', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado'},
+                {id: 1001, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '24 Ene', Usuarios: 290, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'A'},
+                {id: 1002, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '05 Abr', Usuarios: 125, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'B'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 May', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'C'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '14 Jun', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'D'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '18 Jun', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'E'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '30 Ago', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'F'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '02 Sep', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'G'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '19 Sep', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'H'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '24 Oct', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'I'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'J'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'K'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'L'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'M'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'N'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'O'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'P'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'Q'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'R'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'S'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'T'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'U'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'V'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'W'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'X'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'Y'},
+                {id: 1003, Empresa: 'Círculo de crédito S.A de C.V', Fecha: '20 Nov', Usuarios: 235, Publicaciones: 200, Créditos: 'Ilimitado', Directorio: 'Z'}
             ]}
             >   
-            <TableHeader name="Empresa" tooltip="The amazing material name">Empresa</TableHeader>
-            <TableHeader name="Fecha" tooltip="Number of materials">Fecha de regreso</TableHeader>
-            <TableHeader numeric name="Usuarios" tooltip="Price pet unit">Usuarios</TableHeader>
-            <TableHeader name="Publicaciones" tooltip="Number of materials">Publicaciones</TableHeader>
-            <TableHeader name="Créditos" tooltip="Number of materials">Créditos</TableHeader>
+            <TableHeader numeric name="Empresa" tooltip="Empresa">Empresa</TableHeader>
+            <TableHeader name="Fecha" tooltip="Fecha de regreso">Fecha de regreso</TableHeader>
+            <TableHeader name="Usuarios" tooltip="Usuarios">Usuarios</TableHeader>
+            <TableHeader name="Publicaciones" tooltip="Publicaciones">Publicaciones</TableHeader>
+            <TableHeader name="Créditos" tooltip="Créditos">Créditos</TableHeader>
+            <TableHeader numeric name="Directorio"><i class="far fa-star"></i></TableHeader>
         </DataTable>
+        
+        </div>
+
+                    
         </React.Fragment>
         );
     }
